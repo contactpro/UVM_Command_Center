586430c03366e206efd140ef5e2914aa439aab94
Michael Hughes <32088451+contactpro@users.noreply.github.com>
2021-08-26 03:34:33 -0400

    UVM TEST with filename uvm_template_base_test extends uvm_test.sv uvm_template_ files establish a reference design UVM testbench to showcase the commonly utilized features of UVM and SystemVerilog to create automated ASIC/FPGA Design Verification Testbenches.
    UVM TEST with filename uvm_template_base_test extends uvm_test.sv uvm_template_ files establish a reference design UVM testbench to showcase the commonly utilized features of UVM and SystemVerilog to create automated ASIC/FPGA Design Verification Testbenches.

--- uvm_template_base_test.sv
+++ uvm_template_base_test.sv
@@ -13,6 +13,7 @@ import uvm_pkg::*;
 `include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/packet_seq_item.sv"
 `include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/uvm_template_env.sv"
 `include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/uvm_template_sequence.sv"
+`include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/my_uvm_report_server.sv"
 
 class uvm_template_base_test extends uvm_test;
   `uvm_component_utils(uvm_template_base_test)
@@ -24,36 +25,61 @@ class uvm_template_base_test extends uvm_test;
 
   // env instance  
   uvm_template_env env;
-
+  
+  // virtual interface
+  virtual my_if vif;
+  
+  // instantiate modified report server 
+  my_uvm_report_server srv_h;
+  
   // build_phase
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-      `uvm_info(get_type_name(),"In BUILD PHASE . . .", UVM_MEDIUM);  
+      `uvm_info(get_type_name(),"In BUILD PHASE . . .", UVM_MEDIUM);
+        
       // Create the env
       env = uvm_template_env::type_id::create("env", this);
       
+      uvm_config_db#(virtual my_if)::get(this, "", "vif", vif);
+           
+      srv_h = new();
+      uvm_report_server::set_server(srv_h);
+      
   endfunction: build_phase
-  
-  // connect phase
-  virtual function void connect_phase(uvm_phase phase);
-  endfunction: connect_phase  
-  
+   
   // end_of_elaboration_phase 
   virtual function void end_of_elaboration_phase(uvm_phase phase);
     // print the topology
     uvm_top.print_topology();
   endfunction: end_of_elaboration_phase
 
+  function void start_of_simulation_phase(uvm_phase phase);
+    super.start_of_simulation_phase(phase);
+      uvm_report_server::get_server(srv_h);
+      `uvm_info("UVM_TEMPLATE_BASE_TEST",{"this sprint:\n", this.sprint()}, UVM_MEDIUM);
+      `uvm_info("UVM_TEMPLATE_BASE_TEST",{"factory sprint:\n", factory.sprint()}, UVM_MEDIUM);
+  endfunction: start_of_simulation_phase
+  
   // run phase
-  virtual task run_phase (uvm_phase phase);
+  virtual task run_phase(uvm_phase phase);
+    begin 
+      uvm_template_sequence seq; 
+      seq = uvm_template_sequence::type_id::create("seq"); 
+      //-------------------------------------------------------
+      @(posedge vif.clk); 
+      phase.raise_objection(this); 
+      seq.start(env.agnt.sqr); 
+      phase.drop_objection(this); 
+    end
   endtask: run_phase
 
-  // report phase
+  // report phase 
   function void report_phase(uvm_phase phase);
-    uvm_report_server svr;
+    my_uvm_report_server svr;
     super.report_phase(phase);
-   
+      
     svr = uvm_report_server::get_server();
+    
     if(svr.get_severity_count(UVM_FATAL)+svr.get_severity_count(UVM_ERROR)>0) begin
       `uvm_info(get_type_name(), "---------------------------------------", UVM_NONE)
       `uvm_info(get_type_name(), "----            TEST FAIL          ----", UVM_NONE)
@@ -67,5 +93,7 @@ class uvm_template_base_test extends uvm_test;
   endfunction: report_phase 
   
 endclass: uvm_template_base_test
+
+
 
 `endif
