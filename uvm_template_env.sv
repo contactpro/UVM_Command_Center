1e0c91c1d5c590cddaf43eacad9475c1306b00ea
Michael Hughes <32088451+contactpro@users.noreply.github.com>
2021-08-27 10:21:52 -0400

    UVM ENV with filename uvm_template_env extends uvm_env.sv uvm_template_ files establish a reference design UVM testbench to showcase the commonly utilized features of UVM and SystemVerilog to create automated ASIC/FPGA Design Verification Testbenches.
    UVM ENV with filename uvm_template_env extends uvm_env.sv uvm_template_ files establish a reference design UVM testbench to showcase the commonly utilized features of UVM and SystemVerilog to create automated ASIC/FPGA Design Verification Testbenches.

--- uvm_template_env.sv
+++ uvm_template_env.sv
@@ -42,34 +42,36 @@ class uvm_template_env extends uvm_env;
   // build phase 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);   
-      `uvm_info(get_type_name(),"In BUILD PHASE . . .", UVM_MEDIUM);
-      uvm_config_db#(virtual my_if)::get(this, "", "vif", vif);     
-      agnt = uvm_template_agent::type_id::create("agnt", this);
-      scbd = uvm_template_scoreboard::type_id::create("scbd", this);      
+    `uvm_info(get_type_name(),"In ENV BUILD PHASE . . .", UVM_MEDIUM);
+    uvm_config_db#(virtual my_if)::get(this, "", "vif", vif);     
+    agnt = uvm_template_agent::type_id::create("agnt", this);
+    scbd = uvm_template_scoreboard::type_id::create("scbd", this);      
   endfunction: build_phase
   
   // connect phase
   virtual function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
     // connect the scoreboard with the agent
-    agnt.mon.item_collected_port.connect(scbd.item_collected_import);
+    agnt.mon.item_collected_port.connect(scbd.ap_imp);
   endfunction: connect_phase  
 
   // run phase  
   virtual task run_phase (uvm_phase phase);
-    uvm_template_sequence m_seq;
-    m_seq = uvm_template_sequence::type_id::create("m_seq");
+    // uvm_template_sequence m_seq;
+    // m_seq = uvm_template_sequence::type_id::create("m_seq");
     super.run_phase(phase);
-    phase.raise_objection(this);
-    // m_seq.start(uvm_test_top.env.agnt.seqr);
-    // m_seq.start(uvm_template_base_test.env.agnt.seqr);
+    // phase.raise_objection(this);
+    //       m_seq.start(uvm_test_top.env.agnt.seqr);
+    //       m_seq.start(uvm_template_base_test.env.agnt.seqr);
     // -------------------------------------------------------
     // Simulation Error: env.sv(66): 
     // Actual input arg. of type 'reg' for formal 'sequencer' 
     // of 'start' is not compatible with the formal's type 
     // 'class work.uvm_pkg::uvm_sequencer_base'.
-    m_seq.start(env.agnt.seqr);
-    phase.drop_objection(this);
+    // replaced with sequencer start in test class
+    //
+    // m_seq.start(env.agnt.seqr);
+    // phase.drop_objection(this);
   endtask: run_phase
     
 endclass: uvm_template_env
