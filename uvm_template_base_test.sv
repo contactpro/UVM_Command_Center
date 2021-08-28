d3dbc6b059246c545f59272c64119a4fa75fc423
Michael Hughes <32088451+contactpro@users.noreply.github.com>
2021-08-28 10:02:51 -0400

    UVM TEST with filename uvm_template_base_test extends uvm_test.sv
    UVM TEST with filename uvm_template_base_test extends uvm_test.sv uvm_template_ files establish a reference design UVM testbench to showcase the commonly utilized features of UVM and SystemVerilog to create automated ASIC/FPGA Design Verification Testbenches.

--- uvm_template_base_test.sv
+++ uvm_template_base_test.sv
@@ -12,19 +12,14 @@ import uvm_pkg::*;
 `include "C:/Users/HP/WORK_UVM/uvm-1.1d/src/uvm_macros.svh"
 `include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/packet_seq_item.sv"
 `include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/uvm_template_env.sv"
-`include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/my_uvm_report_server.sv"
+`include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/my_uvm_report_server.svh"
 
 class uvm_template_base_test extends uvm_test;
-  `uvm_component_utils(uvm_template_base_test)
-
-  // constructor
-  function new(string name = "uvm_template_base_test", uvm_component parent=null);
-    super.new(name, parent);
-  endfunction: new
   
   uvm_report_server old_server;
+  
   my_uvm_report_server report_server;
-
+  
   // env instance  
   uvm_template_env env;
   
@@ -32,32 +27,56 @@ class uvm_template_base_test extends uvm_test;
   uvm_template_sequence seq;
   
   // virtual interface
-  virtual my_if vif;
-   
+  virtual my_if vif;  
+  
+  uvm_cmdline_processor clp;
+  
+  `uvm_component_utils(uvm_template_base_test)
+
+  // constructor
+  function new(string name = "uvm_template_base_test", uvm_component parent=null);
+    super.new(name, parent);
+    
+    `uvm_info("BASE_TEST_CONSTRUCTOR","In BASE TEST CONSTRUCTOR . . .", UVM_MEDIUM);
+        
+    // Create the env
+    env = uvm_template_env::type_id::create("env", this);
+      
+    // Create the seq
+    seq = uvm_template_sequence::type_id::create("env", this);
+      
+    uvm_config_db#(virtual my_if)::get(this, "", "vif", vif);
+          
+    clp = uvm_cmdline_processor::get_inst();
+    
+  endfunction: new
+  
   // build_phase 
   virtual function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-      `uvm_info("BASE_TEST","In BASE TEST BUILD PHASE . . .", UVM_MEDIUM);
-        
-      // Create the env
-      env = uvm_template_env::type_id::create("env", this);
-      
-      // Create the seq
-      seq = uvm_template_sequence::type_id::create("env", this);
-      
-      uvm_config_db#(virtual my_if)::get(this, "", "vif", vif);
-     
-     `uvm_info("BASE_TEST", "Setting uvm_report_server . . .", UVM_NONE);
+     string clp_uvm_args[$];
+     begin
+     	  // Set the custom report server to output the uvm_info
+     	  // messages in the customized format.
+     	  if (!clp.get_arg_matches("+UVM_REPORT_DEFAULT", clp_uvm_args)) begin
+     	  	  `uvm_info("BASE_TEST", "Testing Command Line Switch . . .", UVM_NONE);
+     	  end // if (!clp.get_arg_matches("+UVM_REPORT_DEFAULT", clp_uvm_args)) begin
+
+        super.build_phase(phase);
+     end
+          
+     // `uvm_info("BASE_TEST", "Setting uvm_report_server to old_server . . .", UVM_NONE);
                
-      old_server = new();
-      uvm_report_server::set_server(old_server);
+      // old_server = new("old_server");
+      // uvm_report_server::set_server(old_server);
       
   endfunction: build_phase
    
   // end_of_elaboration_phase  
   function void end_of_elaboration_phase(uvm_phase phase);
-     report_server = new();
+     `uvm_info("BASE_TEST", "Setting uvm_report_server to report_server. . .", UVM_NONE);   
+     report_server = new("report_server");
      uvm_report_server::set_server(report_server);
+ 	  	
      // print the topology
      uvm_top.print_topology();
   endfunction: end_of_elaboration_phase
