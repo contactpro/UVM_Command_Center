606b90ef48a5f371a81bec60dcbd691ac243083c
Michael Hughes <32088451+contactpro@users.noreply.github.com>
2021-08-28 06:24:14 -0400

    UVM TEST with filename uvm_template_base_test extends uvm_test.sv
    UVM TEST with filename uvm_template_base_test extends uvm_test.sv uvm_template_ files establish a reference design UVM testbench to showcase the commonly utilized features of UVM and SystemVerilog to create automated ASIC/FPGA Design Verification Testbenches.

--- uvm_template_base_test.sv
+++ uvm_template_base_test.sv
@@ -12,14 +12,10 @@ import uvm_pkg::*;
 `include "C:/Users/HP/WORK_UVM/uvm-1.1d/src/uvm_macros.svh"
 `include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/packet_seq_item.sv"
 `include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/uvm_template_env.sv"
-`include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/uvm_template_sequence.sv"
-`include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/uvm_template_sequencer.sv"
 `include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/my_uvm_report_server.sv"
 
 class uvm_template_base_test extends uvm_test;
   `uvm_component_utils(uvm_template_base_test)
-  
-  uvm_template_sequencer sequencer;
 
   // constructor
   function new(string name = "uvm_template_base_test", uvm_component parent=null);
@@ -32,26 +28,26 @@ class uvm_template_base_test extends uvm_test;
   // env instance  
   uvm_template_env env;
   
+  // seq instance
+  uvm_template_sequence seq;
+  
   // virtual interface
   virtual my_if vif;
-  
-  // instantiate modified report server 
-  // my_uvm_report_server srv_h;
-  
+   
   // build_phase 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-      `uvm_info(get_type_name(),"In BUILD PHASE . . .", UVM_MEDIUM);
+      `uvm_info("BASE_TEST","In BASE TEST BUILD PHASE . . .", UVM_MEDIUM);
         
       // Create the env
       env = uvm_template_env::type_id::create("env", this);
       
-      // create sequencer in the test class
-      sequencer = uvm_template_sequencer::type_id::create("uvm_template_sequencer", this);
+      // Create the seq
+      seq = uvm_template_sequence::type_id::create("env", this);
       
       uvm_config_db#(virtual my_if)::get(this, "", "vif", vif);
      
-     `uvm_info("UVM_TEMPLATE_BASE_TEST", "Setting uvm_report_server . . .", UVM_NONE);
+     `uvm_info("BASE_TEST", "Setting uvm_report_server . . .", UVM_NONE);
                
       old_server = new();
       uvm_report_server::set_server(old_server);
@@ -66,23 +62,9 @@ class uvm_template_base_test extends uvm_test;
      uvm_top.print_topology();
   endfunction: end_of_elaboration_phase
 
-  function void start_of_simulation_phase(uvm_phase phase);
-    super.start_of_simulation_phase(phase);
-  endfunction: start_of_simulation_phase
-  
-  // run phase
-  virtual task run_phase(uvm_phase phase);
-    begin 
-    	// these sequencer statements are coded above
-      // uvm_template_sequence seq; 
-      // seq = uvm_template_sequence::type_id::create("seq"); 
-      //-------------------------------------------------------
-      @(posedge vif.clk); 
-      phase.raise_objection(this); 
-      // sequencer.start(env.agnt.seqr); 
-      sequencer.start_phase_sequence(phase); 
-      phase.drop_objection(this); 
-    end
+  // run phase - start the seq on the specified seqr 
+  task run_phase(uvm_phase phase);
+    seq.start(env.agnt.seqr);
   endtask: run_phase
 
   // report phase  
@@ -93,7 +75,7 @@ class uvm_template_base_test extends uvm_test;
             
     super.report_phase(phase);
     
-    `uvm_info("UVM_TEMPLATE_BASE_TEST", "Getting my_uvm_report_server . . .", UVM_NONE);
+    `uvm_info("BASE_TEST", "Getting my_uvm_report_server . . .", UVM_NONE);
      
     svr = uvm_report_server::get_server(); 
     
