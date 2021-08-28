44ee0a1cfb749add248681d5f26c69c6d5e54f1e
Michael Hughes <32088451+contactpro@users.noreply.github.com>
2021-08-28 06:20:57 -0400

    UVM SCOREBOARD with filename uvm_template_scoreboard.sv extends uvm_scoreboard.
    UVM SCOREBOARD with filename uvm_template_scoreboard.sv extends uvm_scoreboard.  uvm_template_ files establish a reference design UVM testbench to showcase the commonly utilized features of UVM and SystemVerilog to create automated ASIC/FPGA Design Verification Testbenches.

--- uvm_template_scoreboard.sv
+++ uvm_template_scoreboard.sv
@@ -13,6 +13,7 @@ import uvm_pkg::*;
 `include "C:/Users/HP/WORK_PYTHON/PY_UVM_TB_BUILDER/uvm_templates/packet_seq_item.sv"
 
 class uvm_template_scoreboard extends uvm_scoreboard;
+  `uvm_component_utils(uvm_template_scoreboard) 
   
   // declaring pkt_qu to store the pkt's received from monitor
   packet_seq_item pkt_qu[$];
@@ -20,13 +21,10 @@ class uvm_template_scoreboard extends uvm_scoreboard;
   // sc_mem 
   bit [7:0] sc_mem [4];
 
-  //port to receive packets from monitor
-  uvm_analysis_imp#(packet_seq_item, uvm_template_scoreboard) ap_imp;
-
-  // factory
-  `uvm_component_utils(uvm_template_scoreboard)
-
-  // constructor
+  //port to receive DUT INPUT (WRITE) and DUT OUTPUT (READ) packets from monitor
+  uvm_analysis_imp #(packet_seq_item, uvm_template_scoreboard) item_collected_export;
+  
+  // constructor 
   function new (string name="uvm_template_scoreboard", uvm_component parent=null);
     super.new(name, parent);
   endfunction : new
@@ -35,7 +33,7 @@ class uvm_template_scoreboard extends uvm_scoreboard;
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
       `uvm_info("SCBD_BUILD_PHASE","In SCBD BUILD PHASE . . .", UVM_MEDIUM);
-      ap_imp = new("ap_imp", this);
+      item_collected_export = new("item_collected_export", this);
       foreach(sc_mem[i]) sc_mem[i] = 8'hFF;
   endfunction: build_phase
   
@@ -49,14 +47,14 @@ class uvm_template_scoreboard extends uvm_scoreboard;
   virtual function void write(packet_seq_item pkt);
     pkt.print();
     pkt_qu.push_back(pkt);
-    `uvm_info("SCBD_PKT_WRITE_TO_QUEUE","SCBD Receives PKT via write method . . .", UVM_MEDIUM);
+    `uvm_info("SCBD_PKT_WRITE_TO_QUEUE","SCBD Receives PKT via push_back from QUEUE . . .", UVM_MEDIUM);
   endfunction : write
 
   // run_phase - compare's the read data with the expected data(stored in local memory)
   // local memory will be updated on the write operation.
   virtual task run_phase(uvm_phase phase);
     packet_seq_item mem_pkt;
-    
+    `uvm_info("SCBD_RUN_PHASE","In SCBD RUN PHASE . . .", UVM_MEDIUM);
     forever begin
       wait(pkt_qu.size() > 0);
       mem_pkt = pkt_qu.pop_front();
