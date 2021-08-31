4398d6ea703f6b14e3b79b3893500feb39332d2b
Michael Hughes <32088451+contactpro@users.noreply.github.com>
2021-08-31 07:48:36 -0400

    UVM Testbench TOP with filename uvm_template_top.sv is the top level testbench module.
    UVM Testbench TOP with filename uvm_template_top.sv is the top level testbench module.  uvm_template_ files establish a reference design UVM testbench to showcase the commonly utilized features of UVM and SystemVerilog to create automated ASIC/FPGA Design Verification Testbenches.

--- uvm_template_top.sv
+++ uvm_template_top.sv
@@ -21,6 +21,22 @@ module uvm_template_top;
   bit clk;
   bit reset;
   
+  bit time_event_0ns;
+  bit time_event_1ns;
+  bit time_event_5ns;
+  bit time_event_10ns;
+  bit time_event_100ns;
+  bit time_event_1000ns;
+  bit time_event_10000ns;
+  
+  bit disable_message_flag_0ns;
+  bit disable_message_flag_1ns;
+  bit disable_message_flag_5ns;
+  bit disable_message_flag_10ns;
+  bit disable_message_flag_100ns;
+  bit disable_message_flag_1000ns;
+  bit disable_message_flag_10000ns;
+    
   // environment class instance
   uvm_template_env env_inst_in_top;
   
@@ -39,20 +55,82 @@ module uvm_template_top;
    );  
   
   
-  //clock generation
+  // clock generation
   always #5ns clk = ~clk;
   
-  //reset generation
+  // reset generation
   initial begin
-    reset = 1ns;
+    reset = 1;
     #30ns reset = 0;
   end
+  
+  // Simulation Time Progress Tracker EVENT BIT initialization. 
+  initial begin
+    time_event_0ns = 0;  	
+    time_event_1ns = 0;
+    time_event_5ns = 0;
+    time_event_10ns = 0;
+    time_event_100ns = 0;
+    time_event_1000ns = 0;
+    time_event_10000ns = 0;
+    disable_message_flag_0ns = 0;
+    disable_message_flag_1ns = 0;
+    disable_message_flag_5ns = 0;
+    disable_message_flag_10ns = 0;
+    disable_message_flag_100ns = 0;
+    disable_message_flag_1000ns = 0;
+    disable_message_flag_10000ns = 0;
+  end
+  
+  // Simulation Time Progress Tracker EVENT BIT SET.
+  always @(posedge clk)
+  begin
+  	if($time == 0ns) time_event_0ns = 1;
+  	if($time > 1ns) time_event_1ns = 1;
+   	if($time > 5ns) time_event_5ns = 1;
+    if($time > 10ns) time_event_10ns = 1;
+    if($time > 100ns) time_event_100ns = 1;
+    if($time > 1000ns) time_event_1000ns = 1;
+    if($time > 10000ns) time_event_10000ns = 1;
+  end				 		
 
+  // Simulation Time Progress Tracker Messages.
+  always @(posedge clk)
+  begin
+  	if((time_event_0ns == 1) && (disable_message_flag_0ns == 0)) begin
+  		disable_message_flag_0ns = 1; 
+  	  `uvm_info("TOP TIME TRACKER","At ZERO Simulation Time at 0ns ",UVM_MEDIUM)
+   	end  	
+  	if((time_event_1ns == 1) && (disable_message_flag_1ns == 0)) begin
+  		disable_message_flag_1ns = 1; 
+  	  `uvm_info("TOP TIME TRACKER","Simulation Time Progress at 1ns ",UVM_MEDIUM)
+   	end
+  	if((time_event_5ns == 1) && (disable_message_flag_5ns == 0)) begin
+   		disable_message_flag_5ns = 1;  		
+  	  `uvm_info("TOP TIME TRACKER","Simulation Time Progress at 5ns ",UVM_MEDIUM)
+   	end
+   	if((time_event_10ns == 1) && (disable_message_flag_10ns == 0)) begin
+  		disable_message_flag_10ns = 1;    		
+  	  `uvm_info("TOP TIME TRACKER","Simulation Time Progress at 10ns ",UVM_MEDIUM)
+   	end
+  	if((time_event_100ns == 1) && (disable_message_flag_100ns == 0)) begin
+  		disable_message_flag_100ns = 1;   		
+  	  `uvm_info("TOP TIME TRACKER","Simulation Time Progress at 100ns ",UVM_MEDIUM)
+   	end
+   	if((time_event_1000ns == 1) && (disable_message_flag_1000ns == 0)) begin
+  		disable_message_flag_1000ns = 1;    		
+  	  `uvm_info("TOP TIME TRACKER","Simulation Time Progress at 1000ns ",UVM_MEDIUM)
+   	end
+    if((time_event_10000ns == 1) && (disable_message_flag_10000ns == 0)) begin
+    	disable_message_flag_10000ns = 1; 
+  	  `uvm_info("TOP TIME TRACKER","Simulation Time Progress at 10000ns ",UVM_MEDIUM)
+   	end  	     	 	   	  	
+  end				 
 
   // enabling the wave dump
   initial begin 
     // uvm_config_db#(virtual my_if)::set(null,"uvm_test_top.env.agnt+","vif",intf);
-    //enable wave dump
+    // enable wave dump
     $dumpfile("dump.vcd"); 
     $dumpvars;
   end
